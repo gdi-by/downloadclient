f68d84978d09a81140c34c9aa0e7c50308523006
Jürgen Weichand <juergen.weichand@ldbv.bayern.de>
2016-03-22 14:24:13 +0100

    Beispiel Java-Klasse für Header aktualisiert

--- examples/java/SomeClass.java
+++ examples/java/SomeClass.java
@@ -1,34 +1,34 @@
-    /*
-     * DownloadClient Geodateninfrastruktur Bayern
-     *
-     * (c) 2016 GSt. GDI-BY (gdi.bayern.de)
-     *
-     * Licensed under the Apache License, Version 2.0 (the "License");
-     * you may not use this file except in compliance with the License.
-     * You may obtain a copy of the License at
-     *
-     *      http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
-    package de.bayern.gdi.downloadclient;
+/*
+ * DownloadClient Geodateninfrastruktur Bayern
+ *
+ * (c) 2016 GSt. GDI-BY (gdi.bayern.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.bayern.gdi.downloadclient;
 
+/**
+ *
+ * @author Juergen Weichand, LDBV Bayern
+ */
+public class SomeClass 
+{
     /**
-     *
-     * @author Juergen Weichand, LDBV Bayern
+     * @param args the command line arguments
      */
-    public class SomeClass 
+    public static void main(String[] args) 
     {
-        /**
-         * @param args the command line arguments
-         */
-        public static void main(String[] args) 
-        {
-            System.out.println("Hello world!");
-        }   
-    }
+        System.out.println("Hello world!");
+    }   
+}
 
