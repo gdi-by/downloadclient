c47091033df438e088aef721dc2be7e86aca596c
Sascha L. Teichmann <teichmann@intevation.de>
2016-05-10 11:46:35 +0200

    Add missing class.

---
+++ src/main/java/de/bayern/gdi/utils/WrapInputStreamFactory.java
@@ -0,0 +1,30 @@
+/*
+ * DownloadClient Geodateninfrastruktur Bayern
+ *
+ * (c) 2016 GSt. GDI-BY (gdi.bayern.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.bayern.gdi.utils;
+
+import java.io.InputStream;
+
+/** Interface to ease the wrapping of input streams. */
+public interface WrapInputStreamFactory {
+
+    /** wrap an inputstream.
+     * @param in the inputstream to wrap.
+     * @return the wrapped inputstream.
+     */
+    InputStream wrap(InputStream in);
+}
