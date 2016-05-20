ef59e39bdc3682fdcec339b7a25e909ff9d071d4
Jochen Saalfeld <jochen@intevation.de>
2016-05-20 10:58:42 +0200

    building javafx-like Layer choosing

--- src/main/java/de/bayern/gdi/gui/WMSMapSwing.java
+++ src/main/java/de/bayern/gdi/gui/WMSMapSwing.java
@@ -24,14 +24,21 @@ import java.net.URL;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import org.geotools.data.ows.Layer;
 import org.geotools.data.wms.WebMapServer;
 import org.geotools.map.MapContent;
 import org.geotools.map.WMSLayer;
 import org.geotools.ows.ServiceException;
 import org.geotools.swing.JMapFrame;
-import org.geotools.swing.wms.WMSLayerChooser;
 
 /**
  * @author Jochen Saalfeld (jochen@intevation.de)
@@ -42,32 +49,117 @@ public class WMSMapSwing extends Parent {
     private static final Logger log
             = Logger.getLogger(WMSMapFX.class.getName());
 
+    private WebMapServer wms;
+    private VBox vBox;
+    private MapContent mapContent;
+    private String title;
+    //private ObservableList<String> layerList;
+    //private ListView wmsLayers;
+    private ComboBox wmsLayers;
+    private Label layerLabel;
+
+    /**
+     * adds a node to this map.
+     * @param n the node
+     */
+    public void add(Node n) {
+        this.vBox.getChildren().remove(n);
+        this.vBox.getChildren().add(n);
+    }
+
+    /**
+     * gets the children of this node.
+     * @return the children of the node
+     */
+    @Override
+    public ObservableList getChildren() {
+        return super.getChildren();
+    }
+
+    /**
+     * Constructor.
+     */
     public WMSMapSwing() {
 
     }
 
+    /**
+     * Constructor.
+     * @param mapURL The URL of the WMS Service
+     * @throws MalformedURLException
+     */
     public WMSMapSwing(String mapURL) throws MalformedURLException {
         this(new URL(mapURL));
     }
 
+    /**
+     * Constructor.
+     * @param mapURL The URL of the WMS Service
+     */
     public WMSMapSwing(URL mapURL) {
         try {
-            WebMapServer wms = new WebMapServer(mapURL);
-            List<Layer> wmsLayers = WMSLayerChooser.showSelectLayer(wms);
-            if (wmsLayers == null) {
+            this.vBox = new VBox();
+            this.wms = new WebMapServer(mapURL);
+            List<Layer> layers = this.wms.getCapabilities().getLayerList();
+            ObservableList<String> layerList = FXCollections
+                    .observableArrayList();
+            for (Layer layer: layers) {
+                layerList.add(layer.getName());
+            }
+            if (layers == null) {
                 throw new ServiceException("could not connect to url");
             }
-            MapContent mapcontent = new MapContent();
-            mapcontent.setTitle(wms.getCapabilities().getService().getTitle());
+            this.mapContent = new MapContent();
+            this.title = this.wms.getCapabilities().getService().getTitle();
+            this.mapContent.setTitle(this.title);
+            this.wmsLayers = new ComboBox(layerList);
+            this.layerLabel = new Label();
+            this.layerLabel.setLabelFor(this.wmsLayers);
+            this.layerLabel.setText("Layer: ");
+            this.add(this.layerLabel);
+            this.add(this.wmsLayers);
+            this.getChildren().add(vBox);
+            this.wmsLayers.setOnAction(new SelectLayer());
 
-            for (Layer wmsLayer : wmsLayers) {
-                WMSLayer displayLayer = new WMSLayer(wms, wmsLayer);
-                mapcontent.addLayer(displayLayer);
-            }
-            // Now display the map
-            JMapFrame.showMap(mapcontent);
+            //Actually select the second entry, because the first is null
+            this.wmsLayers.getSelectionModel().select(0);
+            this.wmsLayers.getSelectionModel().selectNext();
+            //Maually fire the Event, because we "selected" something
+            ActionEvent layerSelected = new ActionEvent();
+            SelectLayer selLay = new SelectLayer();
+            selLay.handle(layerSelected);
         } catch (IOException | ServiceException e) {
             log.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    private void displayMap(Layer wmsLayer) {
+        System.out.println("Selected Layer: " + wmsLayer.getName());
+        WMSLayer displayLayer = new WMSLayer(this.wms, wmsLayer);
+        this.mapContent.addLayer(displayLayer);
+        JMapFrame.showMap(this.mapContent);
+    }
+
+    /**
+     * Event Handler for the Layer Combobox.
+     */
+    private class SelectLayer
+            implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent e) {
+            if (wmsLayers.getSelectionModel().getSelectedItem() != null) {
+                String layerName = (String) wmsLayers.getSelectionModel()
+                        .getSelectedItem();
+                List<Layer> layers = wms.getCapabilities().getLayerList();
+                Layer layer = null;
+                for (Layer lay: layers) {
+                    if (layerName == lay.getName()) {
+                        layer = lay;
+                        break;
+                    }
+                }
+                displayMap(layer);
+            }
         }
     }
 }
