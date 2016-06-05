ae6bab81e2b5a4712d8ea9600a0af14387da7c5c
Sascha L. Teichmann <teichmann@intevation.de>
2016-06-05 15:02:03 +0200

    First models for the 'Verarbeitungskonfiguration'. TODO: create JAXB bindings.

---
+++ src/main/java/de/bayern/gdi/model/InputElement.java
@@ -0,0 +1,76 @@
+/*
+ * DownloadClient Geodateninfrastruktur Bayern
+ *
+ * (c) 2016 GSt. GDI-BY (gdi.bayern.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.bayern.gdi.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** 'Eingabeelement' of processing step configuration. */
+public class InputElement {
+
+    private String name;
+    private String type;
+
+    private List<Option> options;
+
+    public InputElement() {
+        this.options = new ArrayList<>();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the options
+     */
+    public List<Option> getOptions() {
+        return options;
+    }
+
+    /**
+     * @param options the options to set
+     */
+    public void setOptions(List<Option> options) {
+        this.options = options;
+    }
+}
