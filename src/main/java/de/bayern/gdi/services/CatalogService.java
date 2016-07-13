da8cdc0c0c71b450efb5e5ecbcbfdc23b242eebd
Jochen Saalfeld <jochen@intevation.de>
2016-07-13 07:36:54 +0200

    fix checkstyle

--- src/main/java/de/bayern/gdi/services/CatalogService.java
+++ src/main/java/de/bayern/gdi/services/CatalogService.java
@@ -31,14 +31,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.xml.namespace.NamespaceContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathConstants;
 
 import org.apache.commons.io.IOUtils;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
