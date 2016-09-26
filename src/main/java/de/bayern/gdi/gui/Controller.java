9c85f1f2041d4332f5d47c06b46bab307577e0a7
Jochen Saalfeld <privat@jochen-saalfeld.de>
2016-09-26 09:16:07 +0200

    The VBoxes need to be initialized by the UI Factory
    Else they will be unusable and there is no display of UI on the right side.

--- src/main/java/de/bayern/gdi/gui/Controller.java
+++ src/main/java/de/bayern/gdi/gui/Controller.java
@@ -913,9 +913,7 @@ public class Controller {
         this.dataBean.reset();
         this.mapAtom.reset();
         this.mapWFS.reset();
-        this.simpleWFSContainer = new VBox();
         this.simpleWFSContainer.setVisible(false);
-        this.basicWFSContainer = new VBox();
         this.basicWFSContainer.setVisible(false);
         this.mapNodeWFS.setVisible(false);
         this.atomContainer.setVisible(false);
