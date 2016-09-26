1f2a062d2603d6e6dcf3aa6526ae8a0a76f4a632
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2016-09-26 10:26:14 +0200

    Made it compile with style checks on again.

--- src/main/java/de/bayern/gdi/gui/WMSMapSwing.java
+++ src/main/java/de/bayern/gdi/gui/WMSMapSwing.java
@@ -20,7 +20,6 @@ package de.bayern.gdi.gui;
 
 
 import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.geom.impl.PackedCoordinateSequence;
 import de.bayern.gdi.utils.I18n;
 import java.awt.Color;
 import java.awt.Dimension;
