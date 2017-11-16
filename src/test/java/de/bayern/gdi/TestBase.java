fc18fad6a0d1cf82ff904c40284b3996b0ffa39f
Thomas Junk <tjunk@intevation.de>
2017-11-16 19:04:35 +0100

    Basic Refactoring of Issue86Test. Extracted TestBase from Class for future tests. Cleaning up of remaining Testcode.

---
+++ src/test/java/de/bayern/gdi/TestBase.java
@@ -0,0 +1,122 @@
+/*
+ * DownloadClient Geodateninfrastruktur Bayern
+ *
+ * (c) 2016 GSt. GDI-BY (gdi.bayern.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.bayern.gdi;
+
+import de.bayern.gdi.gui.Controller;
+import de.bayern.gdi.gui.DataBean;
+import de.bayern.gdi.gui.Start;
+import de.bayern.gdi.gui.WarningPopup;
+import de.bayern.gdi.utils.I18n;
+import de.bayern.gdi.utils.Config;
+import de.bayern.gdi.utils.Unauthorized;
+import de.bayern.gdi.utils.FileResponseHandler;
+import de.bayern.gdi.utils.DocumentResponseHandler;
+import de.bayern.gdi.utils.Misc;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.junit.BeforeClass;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+
+/*
+ *
+ * @author thomas
+ */
+public class TestBase extends ApplicationTest {
+
+
+    protected static final int WIDTH = 1024;
+    protected static final int HEIGHT = 768;
+    protected static final int TIMEOUT_SECONDS = 15;
+    protected static final int POLL_MILLISECONDS = 250;
+    private static final String LOGONAME = "icon_118x118_300dpi.jpg";
+    protected Scene scene;
+    private Controller controller;
+    private Logger log = Logger.getLogger(
+            this.getClass().getName());
+
+    /**
+     * Initial phase.
+     */
+    @BeforeClass
+    public static void initTests() {
+        System.err.println("init tests ....");
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+    }
+
+    /**
+     * starts the application.
+     *
+     * @param primaryStage the stage
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.err.println("start stage ...");
+        Config.initialize(null);
+
+        log.log(Level.INFO, "Preparing app for controller tests");
+        ClassLoader classLoader = Start.class.getClassLoader();
+        URL url = classLoader.getResource("download-client.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(url, I18n.getBundle());
+        Parent root = fxmlLoader.load();
+        scene = new Scene(root, WIDTH, HEIGHT);
+        controller = fxmlLoader.getController();
+        controller.setDataBean(new DataBean());
+        controller.setPrimaryStage(primaryStage);
+        Unauthorized unauthorized = new WarningPopup();
+        FileResponseHandler.setUnauthorized(unauthorized);
+        DocumentResponseHandler.setUnauthorized(unauthorized);
+        Image image = new Image(Misc.getResource("img/" + LOGONAME));
+        primaryStage.getIcons().add(image);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    protected void waitForStatus(TitledPane titlePane, String status) {
+        await()
+                .atMost(TIMEOUT_SECONDS, SECONDS)
+                .pollInterval(POLL_MILLISECONDS, MILLISECONDS)
+                .until(
+                        () -> titlePane.getText()
+                                .equals(I18n.getMsg(status))
+                );
+    }
+
+
+    protected <T extends javafx.scene.control.Control> T getElementByName(
+            String element, Class<T> type) {
+        return type.cast(scene.lookup(element));
+    }
+
+}
