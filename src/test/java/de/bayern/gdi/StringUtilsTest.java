bc480173346f1d42544bdf6a7b918fcb77adb5ca
Torsten Friebe <torsten.friebe@gmail.com>
2018-12-06 09:32:55 +0100

    #5 Fixed deprecated import

--- src/test/java/de/bayern/gdi/StringUtilsTest.java
+++ src/test/java/de/bayern/gdi/StringUtilsTest.java
@@ -24,7 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Class to test String Utilities.
