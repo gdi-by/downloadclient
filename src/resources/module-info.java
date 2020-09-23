0647a9e46b5da2213cb3e5b26297ff05cb787363
Torsten Friebe <torsten.friebe@gmail.com>
2020-09-23 18:00:47 +0200

    #45 Improved documentation when using Java 11 with JavaFX SDK, fixed packaging of openjfx libraries

--- src/resources/module-info.java
+++
@@ -1,9 +0,0 @@
-module dlcfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.geotools.geometry;
-    requires com.sothawo.mapjfx;
-
-    opens de.bayern.gdi to javafx.fxml;
-    exports de.bayern.gdi;
-}
