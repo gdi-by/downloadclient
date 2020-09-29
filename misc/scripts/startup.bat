111067d1a3908638572b8fd0ea8a3cc0e74effc5
Lyn Elisa Goltz <goltz@lat-lon.de>
2020-09-29 07:41:36 +0200

    #45 - support of Java 11

--- misc/scripts/startup.bat
+++ misc/scripts/startup.bat
@@ -15,6 +15,31 @@ call %dlc%bin\gisinternals\SDKShell.bat setenv
 @echo This may take a while!
 @echo -----------------------------------------------
 
-start javaw -jar downloadclient.jar -config=config
+where java >nul 2>nul
+if %errorlevel%==1 (
+    @echo "No Java Runtime (JRE/JDK) installed! Please read the documentation for further information."
+) else (
+  for /f "tokens=3" %%g in ('java -version 2^>^&1 ^| findstr /i "version"') do (
+    set JAVAVER=%%g
+  )
+  set JAVAVER=%JAVAVER:"=%
+  @echo Java version: %JAVAVER%
 
-pause
+  IF "%JAVAVER:~0,3%"== "1.8" (
+    @echo "Starting GDI-BY Downloadclient using Java version 1.8"
+    start javaw -jar downloadclient.jar -config=config
+    pause
+  ) else (
+    IF "%JAVAVER:~0,4%"== "11.0" (      
+      IF %JAVAFX_HOME% == "" (  
+        @echo "JAVAFX_HOME is not set. Please read the documentation for further information."
+      ) else (
+        @echo "Starting GDI-BY Downloadclient using Java version 11"
+        start javaw -cp downloadclient.jar --module-path %JAVAFX_HOME% --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.web --add-opens=java.base/java.lg=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=javafx.graphics/javafx.application=ALL-UNNAMED --add-opens=javafx.graphics/javafx.geometry=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.sg.prism=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.util=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.logging=ALL-UNNAMED --add-exports javafx.graphics/com.sun.prism=ALL-UNNAMED --add-exports javafx.graphics/com.sun.glass.ui=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.geom.transform=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED --add-exports javafx.graphics/com.sun.glass.utils=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.font=ALL-UNNAMED --add-exports javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.scene.input=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.geom=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.application=ALL-UNNAMED --add-exports javafx.graphics/com.sun.prism.paint=ALL-UNNAMED --add-exports javafx.graphics/com.sun.scenario.effect=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.text=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.iio=ALL-UNNAMED de.bayern.gdi.App
+        pause
+      )
+    ) else (
+      @echo "No suitable Java version found. Please read the documentation for further information."
+    )
+  )
+)
